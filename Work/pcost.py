4981a01fed3ea440430a41964e551dd9e4258bb8
Lunamor <hongyi.meng@jigjog.org>
2021-07-12 17:05:01 -0500

    finished chapter 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -4,6 +4,7 @@
 
 total = 0.00
 with open("Data/portfolio.csv", "rt") as f:
+    print("Exercise 1.27")
     headers = next(f)
     for line in f:
         row = line.split(",")
@@ -13,7 +14,7 @@ print(f"Total cost {total:0.2f}")
 
 
 # Exercise 1.30
-
+'''
 def portfolio_cost(filename):
     'compute the cost of shares'
     total = 0.00
@@ -27,9 +28,9 @@ def portfolio_cost(filename):
 
 cost = portfolio_cost("Data/portfolio.csv")
 print("total cost:", cost)
-
+'''
 # Exercise 1.31: Error handling
-
+'''
 def portfolio_cost(filename):
     'compute the cost of shares'
     total = 0.00
@@ -46,3 +47,52 @@ def portfolio_cost(filename):
 
 cost = portfolio_cost("Data/portfolio.csv")
 print("total cost:", cost)
+'''
+
+# Exercise 1.32: Using a library function
+'''
+import csv
+
+def portfolio_cost(filename):
+    'compute the cost of shares'
+    print('Exercise 1.32')
+    total = 0.00
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                cost = int(row[1]) * float(row[2])
+                total = total + cost
+            except ValueError:
+                print("Error: Some fields are missing")
+    return total
+
+cost = portfolio_cost("Data/portfolio.csv")
+print("total cost:", cost)
+'''
+
+# Exercise 1.33: Reading from the command line
+
+import sys
+import csv
+def portfolio_cost(filename):
+    'compute the cost of shares'
+    print('Exercise 1.33')
+    total = 0.00
+    with open(filename, "rt") as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            try:
+                cost = int(row[1]) * float(row[2])
+                total = total + cost
+            except ValueError:
+                print("Error: Some fields are missing")
+    return total
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
