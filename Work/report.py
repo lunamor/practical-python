5997cc5479a17df992bd53735b10e16398047ce0
Lunamor <hongyi.meng@jigjog.org>
2021-07-13 13:42:50 -0500

    finished exercise 2.7

--- Work/report.py
+++ Work/report.py
@@ -45,4 +45,17 @@ def read_prices(filename):
                 print("Error: no such index")
     return prices
 
+# Exercise 2.7: Fingin out if you can retire
+
+def calc_gain_loss(portfolio_file, current_values_file):
+    'computes the current value of the portfolio along with the gain/loss'
+    portfolio = read_portfolio(portfolio_file)
+    current_prices = read_prices(current_values_file)
+
+    old_portfolio_value = 0.0
+    new_portfolio_value = 0.0
+    for s in portfolio:
+        old_portfolio_value += s["shares"] * s["price"]
+        new_portfolio_value += s["shares"] * current_prices[s["name"]]
+    return old_portfolio_value - new_portfolio_value
     
