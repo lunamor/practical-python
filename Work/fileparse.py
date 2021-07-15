9770f6cc42071b71aaa588ed5bd49aa830de43d7
Lunamor <hongyi.meng@jigjog.org>
2021-07-15 10:51:42 -0500

    finished exercise 3.5

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,13 +1,11 @@
 # fileparse.py
 #
 # Exercise 3.3: Reading CSV Files
-
+'''
 import csv
 
 def parse_csv(filename):
-    '''
-    Parse a CSV file into a list of records
-    '''
+    'Parse a CSV file into a list of records'
     with open(filename) as f:
         rows = csv.reader(f)
         # Read the file headers
@@ -19,16 +17,14 @@ def parse_csv(filename):
             record = dict(zip(headers, row))
             records.append(record)
     return records
-
+'''
 
 # Exercise 3.4: Building a Column Selector
-
+'''
 import csv
 
 def parse_csv(filename, select = None):
-    '''
-    Parse a CSV file into a list of records
-    '''
+    'Parse a CSV file into a list of records'
     with open(filename) as f:
         rows = csv.reader(f)
         # Read the file headers
@@ -47,6 +43,38 @@ def parse_csv(filename, select = None):
             # Filter the row if specific columns were selected
             if indices:
                 row = [row[index] for index in indices]
+            # Make a dictionary
+            record = dict(zip(headers, row))
+            records.append(record)
+    return records
+'''
+
+# Exercise 3.5: Performing Type Conversion
+
+import csv
+
+def parse_csv(filename, select = None, types = None):
+    'Parse a CSV file into a list of records'
+    with open(filename) as f:
+        rows = csv.reader(f)
+        # Read the file headers
+        headers = next(rows)
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set fo headers used for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+        records = []
+        for row in rows:
+            if not row: 	# Skip row with no data
+                continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
             # Make a dictionary
             record = dict(zip(headers, row))
             records.append(record)
