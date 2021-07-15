c96086bcb358b7d6fa1676704dbdded2d273d49c
Lunamor <hongyi.meng@jigjog.org>
2021-07-15 17:25:53 -0500

    finished exercise 4.2

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,16 @@
 # stock.py
 #
 # Exercise 4.1: Objects as Data Structures
+'''
+class Stock:
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+'''
+
+# Exercise 4.2: Adding some Methods
 
 class Stock:
 
@@ -9,4 +19,8 @@ class Stock:
         self.shares = shares
         self.price = price
 
+    def cost(self):
+        return self.shares * self.price
 
+    def sell(self, amt):
+        self.shares -= amt
