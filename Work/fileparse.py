4dc6f1a5f51d10317eed58e04f55c30afd4f91a6
Lunamor <hongyi.meng@jigjog.org>
2021-07-15 10:38:08 -0500

    finished exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -19,3 +19,35 @@ def parse_csv(filename):
             record = dict(zip(headers, row))
             records.append(record)
     return records
+
+
+# Exercise 3.4: Building a Column Selector
+
+import csv
+
+def parse_csv(filename, select = None):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    with open(filename) as f:
+        rows = csv.reader(f)
+        # Read the file headers
+        headers = next(rows)
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set fo headers used for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+        records = []
+        for row in rows:
+            if not row: 	# Skip row with no data
+                continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+            # Make a dictionary
+            record = dict(zip(headers, row))
+            records.append(record)
+    return records
