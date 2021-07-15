14897563d0dfa4becaaf8b7931869b80ba975a33
Lunamor <hongyi.meng@jigjog.org>
2021-07-15 11:50:32 -0500

    finished exercise 3.7

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -82,13 +82,49 @@ def parse_csv(filename, select = None, types = None):
 '''
 
 # Exercise 3.6: Working without Headers
-
+'''
 import csv
 
 def parse_csv(filename, select = None, types = None, has_headers = True):
     'Parse a CSV file into a list of records'
     with open(filename) as f:
         rows = csv.reader(f)
+        if has_headers:
+            # Read the file headers
+            headers = next(rows)
+        # If a column selector was given, find indices of the specified columns.
+        # Also narrow the set fo headers used for resulting dictionaries
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+        records = []
+        for row in rows:
+            if not row: 	# Skip row with no data
+                continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types, row)]
+            if has_headers:
+                # Make a dictionary
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+    return records
+'''
+
+# Exercise 3.7: Picking a different column delimiter
+
+import csv
+
+def parse_csv(filename, select = None, types = None, has_headers = True, delimiter = ","):
+    'Parse a CSV file into a list of records'
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter = delimiter)
         if has_headers:
             # Read the file headers
             headers = next(rows)
