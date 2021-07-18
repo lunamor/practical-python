0ab212067cf988f91474a78bc46020d24de8f220
Lunamor <hongyi.meng@jigjog.org>
2021-07-18 12:06:47 -0500

    finished exercise 6.7

--- Work/follow.py
+++ Work/follow.py
@@ -22,7 +22,7 @@ while True:
 '''
 
 # Exercise 6.6: Using a generator to produce data
-
+'''
 import os
 import time
 
@@ -43,4 +43,31 @@ if __name__ == "__main__":
         price = float(fields[1])
         change = float(fields[4])
         if change < 0:
+            print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
+'''
+
+# Exercise 6.7: Watching your portfolio
+
+import os
+import time
+
+def follow(filename):
+    f = open(filename)
+    f.seek(0, os.SEEK_END)	# Move file pointer 0 bytes from end of file
+    while True:
+        line = f.readline()
+        if line == "":
+            time.sleep(0.1)	# Sleep briefly and retry
+            continue
+        yield line
+
+if __name__ == "__main__":
+    import report
+    portfolio = report.read_portfolio("Data/portfolio.csv")
+    for line in follow("Data/stocklog.csv"):
+        fields = line.split(",")
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if name in portfolio:
             print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
