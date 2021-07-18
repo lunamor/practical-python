5476abd1b2c411ce85f4bf0a67bb106cab2253ff
Lunamor <hongyi.meng@jigjog.org>
2021-07-18 12:02:41 -0500

    finished exercise 6.6

--- Work/follow.py
+++ Work/follow.py
@@ -1,7 +1,7 @@
 # follow.py
 #
 # Exercise 6.5: Monitoring a streaming data source
-
+'''
 import os
 import time
 
@@ -19,3 +19,28 @@ while True:
     change = float(fields[4])
     if change < 0:
         print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
+'''
+
+# Exercise 6.6: Using a generator to produce data
+
+import os
+import time
+
+def follow(filename):
+    f = open(filename)
+    f.seek(0, os.SEEK_END)	# Move file pointer 0 bytes from end of file
+    while True:
+        line = f.readline()
+        if line == "":
+            time.sleep(0.1)	# Sleep briefly and retry
+            continue
+        yield line
+
+if __name__ == "__main__":
+    for line in follow("Data/stocklog.csv"):
+        fields = line.split(",")
+        name = fields[0].strip('"')
+        price = float(fields[1])
+        change = float(fields[4])
+        if change < 0:
+            print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
