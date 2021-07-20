31e878e8828e8ab883524d8db1a9f51e28b07ac2
prophetofthedragon <coltainereborn@gmail.com>
2021-07-20 15:32:17 -0500

    finished exercise 6.14

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -23,7 +23,7 @@ class Portfolio:
 '''
    
 # Exercise 6.3: Making a more proper container
-
+'''
 class Portfolio:
     
     def __init__(self, holdings):
@@ -44,6 +44,37 @@ class Portfolio:
     @property
     def total_cost(self):
         return sum([s.cost for s in self._holdings])
+
+    def tabulate_shares(self):
+        from collections import Counter
+        total_shares = Counter()
+        for s in self._holdings:
+            total_shares[s.name] += s.shares
+        return total_shares 
+'''
+
+# Exercise 6.3: Making a more proper container
+
+class Portfolio:
+    
+    def __init__(self, holdings):
+        self._holdings = holdings
+
+    def __iter__(self):
+        return self._holdings.__iter__()
+
+    def __len__(self):
+        return len(self._holdings)
+
+    def __getitem__(self, index):
+        return self._holdings[index]
+
+    def __contains__(self, name):
+        return any(s.name == name for s in self._holdings)
+
+    @property
+    def total_cost(self):
+        return sum(s.cost for s in self._holdings)
 
     def tabulate_shares(self):
         from collections import Counter
