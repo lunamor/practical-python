f35ee3417413893dc8dd882d2909e7100e5aac18
prophetofthedragon <coltainereborn@gmail.com>
2021-07-20 15:55:59 -0500

    finished exercise 7.3

--- Work/ticker.py
+++ Work/ticker.py
@@ -70,7 +70,7 @@ if __name__ == "__main__":
 '''
 
 # Exercise 6.12: Putting it all together
-
+'''
 from follow import follow
 import csv
 
@@ -115,5 +115,49 @@ if __name__ == "__main__":
     portfolio = report.read_portfolio("Data/portfolio.csv")
     rows = parse_stock_data(follow("Data/stocklog.csv"))
     rows = filter_symbols(rows, portfolio)
+    for row in rows:
+        print(row)
+'''
+
+# Exercise 6.15: Code simplification
+
+from follow import follow
+import csv
+
+def select_columns(rows, indices):
+    for row in rows:
+        yield [row[index] for index in indices]
+
+def convert_types(rows, types):
+    for row in rows:
+        yield[func(val) for func, val in zip(types, row)]
+
+def make_dicts(rows, headers):
+    return (dict(zip(headers, row)) for row in rows)
+
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows = select_columns(rows, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ["name", "price", "change"])
+    return rows
+
+def ticker(portfile, logfile, fmt):
+    import report
+    portfolio = report.read_portfolio(portfile)
+    rows = parse_stock_data(follow(logfile))
+    rows = (row for row in rows if row["name"] in portfolio)
+    import tableformat
+    formatter = tableformat.create_formatter(fmt)
+    formatter.headings(["Name", "Price", "Change"])
+    for row in rows:
+        formatter.row([row['name'], f"{row['price']:0.2f}", f"{row['change']:0.2f}"])
+    
+
+if __name__ == "__main__":
+    import report
+    portfolio = report.read_portfolio("Data/portfolio.csv")
+    rows = parse_stock_data(follow("Data/stocklog.csv"))
+    rows = (row for row in rows if row["name"] in portfolio)
     for row in rows:
         print(row)
