7aa20bf91b5456d510b700a7a4d67e8c5fbfc298
prophetofthedragon <coltainereborn@gmail.com>
2021-07-20 17:40:19 -0500

    finished exercise 7.8

--- Work/stock.py
+++ Work/stock.py
@@ -96,7 +96,7 @@ class Stock:
 '''
 
 # Exercise 5.8: Adding slots
-
+'''
 class Stock:
 
     __slots__ = ("name", "_shares", "price")
@@ -119,6 +119,56 @@ class Stock:
         if not isinstance(value, int):
             raise TypeError("Expected int")
         self._shares = shares
+
+    def sell(self, amt):
+        self.shares -= amt
+    
+    def __repr__(self):
+        return(f"Stock('{self.name}', {self.shares}, {self.price})")
+'''
+
+# Exercise 7.7: Using Closures to Avoid Repetition
+'''
+from typedproperty import typedproperty
+class Stock:
+
+    name = typedproperty("name", str)
+    shares = typedproperty("shares", int)
+    price = typedproperty("price", float)
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self._shares = shares
+        self.price = price
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, amt):
+        self.shares -= amt
+    
+    def __repr__(self):
+        return(f"Stock('{self.name}', {self.shares}, {self.price})")
+'''
+
+# Exercise 7.8: Simplifying Function Calls
+
+from typedproperty import typedproperty
+class Stock:
+
+    name = String(name)
+    shares = Integer("shares")
+    price = Float("price")
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self._shares = shares
+        self.price = price
+
+    @property
+    def cost(self):
+        return self.shares * self.price
 
     def sell(self, amt):
         self.shares -= amt
